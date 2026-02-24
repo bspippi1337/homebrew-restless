0125c5821960f3d9037b90af0b2c018b80b41781
bspippi1337 <bspippi1337@gmail.com>
2026-02-24 02:59:49 +0100

    restless v0.2.2-2-gb749d65-13-gfa10c9a-12-g67b309a

--- Formula/restless.rb
+++ Formula/restless.rb
@@ -1,11 +1,11 @@
 class Restless < Formula
   desc "Restless adaptive API client"
   homepage "https://github.com/bspippi1337/restless"
-  version "v0.2.2-2-gb749d65-13-gfa10c9a"
+  version "v0.2.2-2-gb749d65-13-gfa10c9a-12-g67b309a"
 
   on_linux do
-    url "https://github.com/bspippi1337/restless/releases/download/vv0.2.2-2-gb749d65-13-gfa10c9a/restless_v0.2.2-2-gb749d65-13-gfa10c9a_linux_amd64.tar.gz"
-    sha256 "d43fe2ee06c0fcc4775749cb47102c34a7286a0e6316683a43dfb748fb9e1eb2"
+    url "https://github.com/bspippi1337/restless/releases/download/vv0.2.2-2-gb749d65-13-gfa10c9a-12-g67b309a/restless_v0.2.2-2-gb749d65-13-gfa10c9a-12-g67b309a_linux_amd64.tar.gz"
+    sha256 "9a507a911d5657dfa46a5d6298bdda02333cbe7eaed4a7a86281a09e7e9ae1f9"
     def install
       bin.install "restless_linux_amd64" => "restless"
     end
