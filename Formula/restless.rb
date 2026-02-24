4b0e267de9b2f28e9a1dc25baed126e08da23044
Bill Gates <billgates@microsoft.com>
2026-02-24 01:33:37 +0100

    restless v0.2.2-2-gb749d65-13-gfa10c9a

--- Formula/restless.rb
+++ Formula/restless.rb
@@ -1,11 +1,11 @@
 class Restless < Formula
   desc "Restless adaptive API client"
   homepage "https://github.com/bspippi1337/restless"
-  version "v0.2.2-2-gb749d65"
+  version "v0.2.2-2-gb749d65-13-gfa10c9a"
 
   on_linux do
-    url "https://github.com/bspippi1337/restless/releases/download/vv0.2.2-2-gb749d65/restless_v0.2.2-2-gb749d65_linux_amd64.tar.gz"
-    sha256 "fd41741944e5ee8f40d752d8cd5bbaf09b3244f6f5542facf9681f2d3aa98889"
+    url "https://github.com/bspippi1337/restless/releases/download/vv0.2.2-2-gb749d65-13-gfa10c9a/restless_v0.2.2-2-gb749d65-13-gfa10c9a_linux_amd64.tar.gz"
+    sha256 "d43fe2ee06c0fcc4775749cb47102c34a7286a0e6316683a43dfb748fb9e1eb2"
     def install
       bin.install "restless_linux_amd64" => "restless"
     end
